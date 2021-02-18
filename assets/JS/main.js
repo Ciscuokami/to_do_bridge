0c2d973eb6350f3ace5291d0f96f5658ebcb36bc
lulaCodes <luz@diga-33.com>
2021-02-18 21:44:57 +0100

    Crear tareas

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -15,3 +15,26 @@ async function createTask() {
         body: JSON.stringify(obj)
     })
 }
+
+/*
+=====================================
+  USUARIOS
+=====================================
+*/
+
+// Crear usuario
+async function createUser() {
+    const form = document.getElementById("createUser");
+    var obj = {};
+    for (var i = 0; i < form.elements.length; i++) {
+        var item = form.elements.item(i);
+        obj[item.name] = item.value;
+    }
+    const response = await fetch("http://localhost:8080/user", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj)
+    })
+}
